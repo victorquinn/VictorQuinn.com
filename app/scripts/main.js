da7869128345923ad5fcbfd7f62e070ad6500365
Victor Quinn <victor@socialradar.com>
2014-10-11 15:12:29 -0400

    More progress on styles, added Segment

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+var em = document.getElementById('em'), var1 = 'ctorquinn.com', var2 = 'mai', var3 = 'l@vi';
+em.innerHTML = var2 + var3 + var1;
+em.href = var2 + 'lto:' + var2 + var3 + var1 + '?subject=Hello!';
+
+
+var ph = document.getElementById('ph'), var4 = '3) 962', var5 = '(70', var6 = '-7234';
+ph.innerHTML = var5 + var4 + var6;
+ph.href = 'tel:14136872928';
