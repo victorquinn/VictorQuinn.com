39c70e333d3afacb8adbb52ab3ca6dde08991134
Victor Quinn <victor@socialradar.com>
2014-10-11 21:56:17 -0400

    Now with sticky headers and a motorcycle pic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var em = document.getElementById('em'), var1 = 'ctorquinn.com', var2 = 'mai', var3 = 'l@vi';
 em.innerHTML = var2 + var3 + var1;
 em.href = var2 + 'lto:' + var2 + var3 + var1 + '?subject=Hello!';
@@ -6,3 +8,13 @@ em.href = var2 + 'lto:' + var2 + var3 + var1 + '?subject=Hello!';
 var ph = document.getElementById('ph'), var4 = '3) 962', var5 = '(70', var6 = '-7234';
 ph.innerHTML = var5 + var4 + var6;
 ph.href = 'tel:14136872928';
+
+$(function() {
+    $('body').stacks({
+        body: '.maincontent',
+        title: '.sectionheader',
+        offset: 0,
+        margin: 0
+    });
+    console.log('stacks loaded');
+});
